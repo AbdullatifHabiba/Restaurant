e5a4e1ca92af020cfe6b92fbe1e66e2388f0b8f0
Ahmed Ezz <95645898+A7med3z@users.noreply.github.com>
2022-11-28 01:08:00 +0200

    first trial

--- Backend/controller/config.ts
+++ Backend/controller/config.ts
@@ -1,1 +1,11 @@
-console.log("well come")
+import { CustomerServices } from "../services/CustomerService";
+
+export class config {
+    static sign_in(user_name : string, password : string) {
+        return CustomerServices.check_password(user_name, password)
+    }
+
+    static sign_up () {
+        
+    }
+}
--- Backend/controller/route.ts
+++ Backend/controller/route.ts
@@ -1,2 +1,17 @@
-
 import express from 'express'
+import { config } from './config'
+
+const app = express()
+
+app.listen(3000);
+app.use(express.json());
+
+// GET method route
+app.get('/', (req, res) => {
+    res.send(config.sign_in(req.body.user_name, req.body.password))
+})
+  
+  // POST method route
+app.post('/', (req, res) => {
+    res.send(req.body.id)
+})
