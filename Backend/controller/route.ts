faec907eb92d667f5857b426d309aa62b89096ff
Ahmed Ezz <95645898+A7med3z@users.noreply.github.com>
2022-12-01 16:51:26 +0200

    satge merge

---
+++ Backend/controller/route.ts
@@ -0,0 +1,37 @@
+import { rejects } from 'assert';
+import express from 'express'
+import { SignUp } from '../repository/SignupRepo';
+import { menuserice } from '../services/MenuService';
+import { signinservice } from '../services/SignInService';
+import { signupservice } from '../services/SignUpService';
+
+
+const app = express()
+app.use(express.json());
+
+const sign_inservice = new signinservice();
+const sign_upservice = new signupservice();
+const menu_service = new menuserice();
+
+// GET method route
+app.get('/signin', (req, res) => {
+  let r = sign_inservice.sign_in(req.body);
+  r.then((accepted) => res.status(200).send(accepted)).catch((rejected) => res.status(404).send(rejected));
+})
+
+app.get('/menu', (req, res) => {
+  let r = menu_service.serve(req.body);
+})
+
+app.get('/homemenu', (req, res) => {
+  let r = menu_service.ge;
+})
+
+const signup = new SignUp();
+// POST method route
+app.post('/signup', (req, res) => {
+  let r = sign_upservice.sign_up(req.body);
+  r.then((accepted) => res.status(200).send(accepted)).catch((rejected) => res.status(404).send(rejected));
+})
+
+app.listen(5000);
---
+++ Backend/services/MenuService.ts
@@ -0,0 +1,9 @@
+import {MenueRepo} from "../repository/MenueRepo"
+
+export class menuserice {
+    menuRepo = new MenueRepo();
+
+    public get6() {
+        
+    }
+}
