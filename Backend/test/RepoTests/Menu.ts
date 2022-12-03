4513bc81daf06bdffbeaf781862f9e121e444c64
MohamedElgreatly <mohamed.ahmed.elgreatly@gmail.com>
2022-12-03 14:23:15 +0200

    after tring some edit in test classes

--- Backend/test/RepoTests/Menu.ts
+++ Backend/test/RepoTests/Menu.ts
@@ -10,7 +10,7 @@
 
 //   it("check added item is ok", async () => {
 //     let result;
-//     await obj.AddItem("meatMeal2", 5, "meat.png", "this meal for me", '25').then(
+//     await obj.AddItem("meatMeal15", 5, "meat.png", "this meal for me", '25').then(
 //       (accepted) => result = accepted,
 //       (rejected) => console.log("Erron" + rejected));
 //     expect(result).to.equals("item added correctly");
@@ -18,7 +18,7 @@
 
 //   it("check added item is exist not to be added", async () => {
 //     let result: any;
-//     await obj.AddItem("riceMeal1", 45, "rice.png", "this meal for me", '5').then(
+//     await obj.AddItem("riceMeal189", 45, "rice.png", "this meal for me", '5').then(
 //       (accepted) => result = accepted,
 //       (rejected) => console.log("Erron" + rejected));
 //     expect(result).to.equals("item added correctly");
