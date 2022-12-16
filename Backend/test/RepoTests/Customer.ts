82f457cd17bb313c0592d18382ef3961b961023e
MohamedElgreatly <mohamed.ahmed.elgreatly@gmail.com>
2022-12-16 17:53:41 +0200

    after enable customer to get all Available items

--- Backend/test/RepoTests/Customer.ts
+++ Backend/test/RepoTests/Customer.ts
@@ -1,16 +1,16 @@
 import { expect } from "chai";
-import { AdminRepo } from "../../repository/AdminRepo";
+import { customerRepo } from "../../repository/CustomerRepo";
 
-describe("Admin Repo Tests", () => {
-  const obj = new AdminRepo();
+describe("Customer Repo Tests", () => {
+  const obj = new customerRepo();
 
   it("shoud return void", () => {
-    expect(obj.getALL()).to.be.a("promise");
+    expect(obj.get_Available_Items()).to.be.a("promise");
   });
 
   it("check length of item returned", async () => {
     let result:any;
-    await obj.getALL().then(
+    await obj.get_Available_Items().then(
       (accepted) => result=accepted,
       (rejected) => console.log("Erron" + rejected));
       console.log(result);
--- Backend/test/ServiceTest/Customer.ts
+++ Backend/test/ServiceTest/Customer.ts
@@ -1,16 +1,16 @@
 import { expect } from "chai";
-import { AdminRepo } from "../../repository/AdminRepo";
+import { CustomerServices } from "../../services/CustomerService";
 
-describe("Admin Repo Tests", () => {
-  const obj = new AdminRepo();
+describe("Customer Service Tests", () => {
+  const obj = new CustomerServices();
 
   it("shoud return void", () => {
-    expect(obj.getALL()).to.be.a("promise");
+    expect(obj.get_Available_Items()).to.be.a("promise");
   });
 
   it("check length of item returned", async () => {
     let result:any;
-    await obj.getALL().then(
+    await obj.get_Available_Items().then(
       (accepted) => result=accepted,
       (rejected) => console.log("Erron" + rejected));
       console.log(result);
