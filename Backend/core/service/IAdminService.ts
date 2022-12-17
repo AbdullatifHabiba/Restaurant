6e50e44538c6dfd42ec448400e8fcb5fc36c4a4f
Ahmed Ezz <95645898+A7med3z@users.noreply.github.com>
2022-12-17 14:12:20 +0200

    making order finished

--- Backend/core/service/IAdminService.ts
+++ Backend/core/service/IAdminService.ts
@@ -1,4 +1,6 @@
 export interface IAdminService {
-    getAllItems():Promise<any>;
-    AddItem_toDB_and_s3(file:any,body:any):Promise<any>;
+    
+    getAllItems(): Promise<any>;
+    
+    AddItem_toDB_and_s3(file: any, body: any): Promise<any>;
 }
--- Backend/services/CustomerService.ts
+++ Backend/services/CustomerService.ts
@@ -1,26 +1,44 @@
 import { ICustomerService } from "../core/service/ICustomerService";
 import { customerRepo } from "../repository/CustomerRepo"
+import { OrderRepo } from "../repository/OrderRepo";
 
 
 export class CustomerServices implements ICustomerService {
     customerRepo = new customerRepo();
+    order_repo = new OrderRepo();
     get_Available_Items() {
         return this.customerRepo.get_Available_Items();
     }
 
-    make_new_order(req:any) {
+    async make_new_order(req: any) {
+        let create = this.order_repo.create_order(req.customer_id, req.price);
+        create.then((accepted) => {
+            if (accepted.state === "accepted") {
+                let id: number = accepted.id;
+                for (let i = 0; i < req.items.length; i++) {
+                    this.order_repo.order_item_repo.create_orderItem(id, req.items[i].id, req.items[i].count, req.items[i].price);
+                }
+                const response = {
+                    id: id,
+                    state: "accepted"
+                };
+                return response;
+            } else {
+                const response = {
+                    state: "can't create order"
+                };
+                return response;
+            }
+        }).catch((rejected) => { return rejected });
+    }
+
+    edit_order(req: any) {
         return new Promise<void>((resolve, reject) => {
             null
         });
     }
 
-    edit_order (req:any) {
-        return new Promise<void>((resolve, reject) => {
-            null
-        });
-    }
-
-    delete_order (req:any) {
+    delete_order(req: any) {
         return new Promise<void>((resolve, reject) => {
             null
         });
