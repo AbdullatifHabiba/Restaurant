2c7545993b9ca04d4088a637f4cd4ec309d98772
Mahmoud-Ramzy <anatedo4545@gmail.com>
2022-12-26 19:44:36 +0200

    RES-49 backend merge

--- Frontend/restaurant/src/OrdersToDelivery/Components/OrderCard.js
+++ Frontend/restaurant/src/OrdersToDelivery/Components/OrderCard.js
@@ -10,7 +10,7 @@ export default function OrderCard(props) {
       Id: props.deliverID,
       OrderID: props.OrderID,
     };
-    NAV("/orders", { state: NewOrder });
+    NAV("/orders", { state: NewOrder }); //orders --> ayman page
   }
 
   return (
--- Frontend/restaurant/src/OrdersToDelivery/OrdersDelivery.js
+++ Frontend/restaurant/src/OrdersToDelivery/OrdersDelivery.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import OrderCard from "./Components/OrderCard";
 import { environment } from "../environment";
 import "./order.css";
 export default function OrdersDelivery() {
   const location = useLocation();
   let [Orders, setOrders] = React.useState([]);
+  /*
+      {
+      payment: "Order1",
+      price: 47,
+      address: "15 tahreer street, cairo",
+      order_id: "11111111",
+    },
+  */
+
   React.useEffect(() => {
     async function getOrders() {
-      let result = await fetch(`${environment.env}/OrdersDelivery`, {
+      let result = await fetch(`${environment.env}/orders`, {
         method: "get",
         headers: {
           "Content-type": "application/json",
@@ -22,13 +31,12 @@ export default function OrdersDelivery() {
   let OrdersHTML = Orders.map((O) => {
     return (
       <OrderCard
-        name={O.name}
+        name={O.payment}
         price={O.price}
-        Count={O.Count}
         address={O.address}
         deliver={location.state.name}
         deliverID={location.state.id}
-        OrderID={O.OrderID}
+        OrderID={O.order_id}
       />
     );
   });
